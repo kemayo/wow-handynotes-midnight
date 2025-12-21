1a6bf1e0943408264d358bf0b8371372b34827dd
David Lynch <kemayo@gmail.com>
2025-12-21 12:05:40 -0600

    Lore Hunter: skeleton

--- achievements/LoreHunter.lua
+++ achievements/LoreHunter.lua
@@ -1,3 +1,22 @@
 local myname, ns = ...
 
--- 62104
+local LORE = {
+    achievement=62104,
+    texture=ns.atlas_texture("profession", {r=1, g=1, b=0}),
+    -- minimap=true,
+}
+
+ns.RegisterPoints(ns.EVERSONGWOODS, {
+}, LORE)
+
+ns.RegisterPoints(ns.ZULAMAN, {
+
+}, LORE)
+
+ns.RegisterPoints(ns.HARANDAR, {
+
+}, LORE)
+
+ns.RegisterPoints(ns.VOIDSTORM, {
+
+}, LORE)
