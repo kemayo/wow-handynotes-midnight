87076ac1b02abbf7bb51115c521c363b6dc757a5
David Lynch <kemayo@gmail.com>
2025-12-31 15:53:27 -0600

    Loot for world bosses in Eversong and Zul'aman

---
+++ shared/WorldBosses.lua
@@ -0,0 +1,41 @@
+local myname, ns = ...
+
+local boss = {
+    group="worldboss",
+}
+
+ns.RegisterPoints(ns.EVERSONGWOODS, {
+    [45245997] = { -- Lu'ashal
+        npc=244762,
+        quest=92560,
+        worldquest=92560,
+        loot={
+            250447, -- Radiant Eversong Scepter
+            250451, -- Dawncrazed Beast Cleaver
+            250453, -- Scepter of the Unbound Light
+            250456, -- Wretched Scholar's Gilded Robe
+            250457, -- Devouring Outrider's Chausses
+            250458, -- Host Commander's Casque
+            250459, -- Bramblestalker's Feathered Cowl
+            250462, -- Forgotten Farstrider's Insignia
+        },
+    }
+}, boss)
+
+ns.RegisterPoints(ns.ZULAMAN, {
+    [45244790] = { -- Cragpine
+        quest=92123,
+        worldquest=92123,
+        npc=244424,
+        loot={
+            250446, -- Cragtender Bulwark
+            250450, -- Forest Sentinel's Savage Longbow
+            250456, -- Wretched Scholar's Gilded Robe
+            250457, -- Devouring Outrider's Chausses
+            250458, -- Host Commander's Casque
+            250459, -- Bramblestalker's Feathered Cowl
+            250461, -- Chain of the Ancient Watcher
+            250462, -- Forgotten Farstrider's Insignia
+        },
+    },
+}, boss)
