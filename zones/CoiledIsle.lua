0314a32d04c966c206aa89eb03c3a0b7e0ef0442
David Lynch <kemayo@gmail.com>
2026-08-06 00:37:24 -0500

    Coiled Isle: treasures

--- zones/CoiledIsle.lua
+++ zones/CoiledIsle.lua
@@ -5,31 +5,142 @@ TODO:
 63382 It's Definitely Something
 --]]
 
+local ZULJARRA = ns.rewards.Currency(ns.CURRENCY_ZULJARRA, 50)
+
 -- Treasures
 
 ns.RegisterPoints(ns.COILEDISLE, {
-	-- [] = {criteria=115289, quest=94569, vignette=7480}, -- Amani Privateer's Cache
-	-- [] = {criteria=115313, quest=95938, vignette=7604}, -- Fangbound Sack
-	-- [] = {criteria=115290, quest=95907, vignette=7599}, -- Sunken Diver's Chest
-	-- [] = {criteria=115291, quest=95956, vignette=7608}, -- Grave of Someone Forgotten
-	-- [] = {criteria=115292, quest=95941, vignette=7607}, -- Profane Ritual Spoils
-	-- [] = {criteria=115294, quest=95995, vignette=7627}, -- Brine-Crusted Chest
-	-- [] = {criteria=115295, quest=96985, vignette=7743}, -- Possessed Vase
-	-- [] = {criteria=115306, quest=95164, vignette=7532}, -- Malfunctioning Staff
-	-- [] = {criteria=115307, quest=95563, vignette=7559}, -- Tarnished Amani Glaive
-	-- [] = {criteria=115309, quest=95566, vignette=7560}, -- Jaktu's Cursed Blade
-	-- [] = {criteria=115310, quest=95571, vignette={7561, 7696}}, -- Lost Spirit (A Mysterious Urn, then the spirit)
-	-- [] = {criteria=115312, quest=95594, vignette=7563}, -- Cracked Skull
-	-- [] = {criteria=115296, quest=95596, vignette=7564}, -- Damaged Loa Trinket
-	-- [] = {criteria=115298, quest=95835, vignette=7587}, -- Venomjade Necklace
-	-- [] = {criteria=115299, quest=95836, vignette=7589}, -- Ornate Bottle
-	-- [] = {criteria=115300, quest=95841, vignette=7590}, -- Stinking Vessel
-	-- [] = {criteria=115301, quest=95854, vignette=7591}, -- Waterlogged Basket
-	-- [] = {criteria=115302, quest=95855, vignette=7592}, -- Smoldering Incense
-	-- [] = {criteria=115308, quest=95558, vignette=7557}, -- Crumbling Urn
-	-- [] = {criteria=115314, quest=95591, vignette=7562}, -- Forgotten Mask
-	-- [] = {criteria=115293, quest=95976, vignette=7610}, -- Vul'zahn's Smuggled Treasure
-	-- [] = {criteria=115297, quest=95727, vignette=7577}, -- Zul'jan's Stash
+	[71886666] = { -- Amani Privateer's Cache
+		criteria=115289, quest=94569,
+		loot={
+			{279054, toy=true}, -- Idol of Blue Water and Blue Sky
+			ZULJARRA
+		},
+		note="Fish up a {item:265525:Grisly Morsel}, give it to the {npc:258076:Hungry Dolphin}, find the two halves of the key and make the {item:265602:Amani Privateer's Key}",
+		related={
+			[73426614] = {label="Grisly Cod Pool", atlas="Professions_Tracking_Fish_Special", loot={265525}},
+			[72516723] = {label="{npc:258076:Hungry Dolphin}", note="Give the {item:265525:Grisly Morsel}", hide_before=ns.conditions.Item(265525)},
+			[72416841] = {label="Broken Urn", loot={265603}, hide_before=ns.conditions.AuraActive(1275730)},
+			[73096698] = {label="Waterlogged Crate", loot={265610}, hide_before=ns.conditions.AuraActive(1275730)},
+			minimap=true, worldmap=false,
+		},
+		vignette=7480, -- Privateer's Cache
+		atlas="VignetteLootElite", scale=1.1,
+	},
+	[45916628] = {criteria=115313, quest=95938, loot={281571, ZULJARRA}, vignette=7604}, -- Fangbound Sack, Focus of Fangs
+	[65440560] = { -- Sunken Diver's Chest
+		criteria=115290, quest=95907,
+		loot={
+			{279052, toy=true}, -- Ancient Amani Mask
+			ZULJARRA,
+		},
+		note="Kill nearby {npc:263081:Glittering Grouper Brintail} for {item:271424:Diver's Key Fragment}x3",
+		vignette=7599,
+		atlas="VignetteLootElite", scale=1.1,
+	},
+	[67264846] = { -- Grave of Someone Forgotten
+		criteria=115291, quest=95956,
+		loot={
+			{279021, toy=true}, -- Forgotten Memento
+			ZULJARRA
+		},
+		note="Read the Nameless Grave, find other nearby ghosts that you can ask about the name, return to the grave",
+		related={
+			-- extreme lack of helpful quest progression here; they just stand up and walk away
+			[69045270] = {label="{npc:263241:Zuzan}"},
+			-- [70405840] = {label="{npc:263242:Zan'ja}"}, -- wowhead swears, but I didn't talk to this one...
+			[66425725] = {label="{npc:263243:Ru'ko}"},
+			minimap=true, worldmap=true,
+		},
+		vignette=7608,
+		atlas="VignetteLootElite", scale=1.1,
+	},
+	[43646738] = { -- Profane Ritual Spoils (Ritual Chest)
+		criteria=115292, quest=95941,
+		loot={
+			281567, -- Profane Ritual Staff
+			ns.rewards.Currency(ns.CURRENCY_VOIDLIGHT, 250),
+			ZULJARRA,
+		},
+		note="Use {npc:263202:Mysterious Trinket}: upper right, upper left, bottom right, bottom left",
+		vignette=7607, -- Ritual Chest
+		atlas="VignetteLootElite", scale=1.1,
+	},
+	[70637663] = { -- Brine-Crusted Chest
+		criteria=115294, quest=95995, -- 96001 pearl dropped, 96002 key dropped
+		loot={{274921, toy=true}, ZULJARRA}, -- Pearl of Jubilation
+		note="Find a Bubbling Clam underwater outside the cave; bring a {item:271815:Luminescent Pearl} and put it down for {npc:263347:Nacretta}; take the {item:271881:Dropped Key}; open the chest",
+		vignette=7627, -- Ancient Amani Chest
+		atlas="VignetteLootElite", scale=1.1,
+	},
+	[31438349] = {criteria=115295, quest=96985, loot={279051, ZULJARRA}, vignette=7743}, -- Possessed Vase
+	[75376833] = {criteria=115306, quest=95164, loot={{268504, toy=true}, ZULJARRA}, vignette=7532}, -- Malfunctioning Staff
+	[55213796] = {criteria=115307, quest=95563, loot={275920, ZULJARRA}, vignette=7559}, -- Tarnished Amani Glaive
+	[60435946] = {criteria=115309, quest=95566, loot={{277954, toy=true}, ZULJARRA}, vignette=7560}, -- Jaktu's Cursed Blade
+	[68056590] = { -- Lost Spirit (A Mysterious Urn, then the spirit)
+		criteria=115310, quest=95571, -- 95574 after giving trinket
+		loot={
+			274493, -- Effigy of Ula'tek's Faithful
+			{244345, decor=true}, -- Forgotten Amani Urn
+			ZULJARRA,
+		},
+		vignette={7696, 7561}, -- Lost Spirit, Mysterious Urn
+		related={
+			[70226447] = {label="{item:269935:Forgotten Trinket}", note="Bring to the Lost Spirit", minimap=true},
+		},
+	},
+	[58144355] = {criteria=115312, quest=95594, loot={278031, ZULJARRA}, vignette=7563}, -- Cracked Skull, Cracked Amani Skull
+	[44862957] = {criteria=115296, quest=95596, loot={278032, ZULJARRA}, vignette=7564}, -- Damaged Loa Trinket
+	[64723664] = {criteria=115298, quest=95835, loot={279011, {271175, decor=true}, ZULJARRA}, vignette=7587}, -- Venomjade Necklace (both of them)
+	[66952803] = {criteria=115299, quest=95836, loot={278035, ZULJARRA}, vignette=7589}, -- Ornate Bottle, Ornate Healing Potion (Potent x5)
+	[53094310] = { -- Stinking Vessel
+		criteria=115300, quest=95841,
+		loot={
+			{281580, decor=true}, -- Pungent Atal'Utek Shroom
+			ns.rewards.Item(260290, 5), -- Quel'Thalas Cheese
+			ns.rewards.Currency(ns.CURRENCY_VOIDLIGHT, 100),
+			ZULJARRA,
+		},
+		vignette=7590, -- Intact Vase
+	},
+	[49483198] = { -- Waterlogged Basket
+		criteria=115301, quest=95854,
+		loot={
+			281569, -- Quiver of the Drowned Marksman
+			ns.rewards.Item(258138, 5), -- Potent Healing Potion
+			ZULJARRA,
+		},
+		vignette=7591,
+	},
+	[29546723] = {criteria=115302, quest=95855, loot={281570, ns.rewards.Currency(ns.CURRENCY_VOIDLIGHT, 100), ZULJARRA}, vignette=7592}, -- Smoldering Incense, Amani Incense Stick
+	[73485654] = { -- Crumbling Urn
+		criteria=115308, quest=95558,
+		loot={
+			{281582, decor=true}, -- Atal'Utek Ivy
+			269861, -- A Tattered Amani Scroll
+			269868, -- Miniature Hand-Crafted Mask
+			ZULJARRA,
+		},
+		vignette=7557, -- Broken Amani Urn
+	},
+	[64917889] = {criteria=115314, quest=95591, loot={278003, ZULJARRA}, vignette=7562}, -- Forgotten Mask
+	[58194572] = { -- Vul'zahn's Smuggled Treasure
+		criteria=115293, quest=95976,
+		loot={
+			281568, -- Vul'zahn's Smuggled Spear
+			ns.rewards.Item(258138, 10), -- Potent Healing Potion
+			ZULJARRA
+		},
+		note="{npc:263265:Vul'zahn} wants food; get medicine from {npc:253837:Apothecary Dezi}, give to {npc:262204:Witherbark Cook}, bring food back",
+		related={
+			[57204846] = {label="{npc:253837:Apothecary Dezi}", loot={271791}}, -- Potion of Headache Relief
+			[58004880] = {label="{npc:262204:Witherbark Cook}", loot={271788}, hide_before=ns.conditions.Item(271791)}, -- Snuffling Boar Stew
+			minimap=true, worldmap=true,
+		},
+		vignette=7610,
+		atlas="VignetteLootElite", scale=1.1,
+	},
+	[43952649] = {criteria=115297, quest=95727, loot={281566, ns.rewards.Currency(ns.CURRENCY_VOIDLIGHT, 150), ZULJARRA}, vignette=7577}, -- Zul'jan's Stash, Amani Warrior's Cleaver
 }, {
 	achievement=63359, -- Treasures of the Coiled Isle
 })
