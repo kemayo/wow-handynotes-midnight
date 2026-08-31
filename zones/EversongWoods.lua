kemayo/wow-handynotes-midnight

commit a934612a7e460b389f060d7d57fe8a7008e4246f

Eversong Woods: move Bloated Snapdragon during void invasions

zones/EversongWoods.lua
@@ -126,17 +126,6 @@ ns.RegisterPoints(ns.EVERSONGWOODS, {
 		},
 		vignette=7300, -- Dormant Lightbloom Hydra
 	},
-	[36566407] = { -- Bloated Snapdragon
-		criteria=110169, quest=92366,
-		npc=250582,
-		loot={
-			264543, -- Snapdragon Pantaloons
-			264560, -- Sharpclaw Gauntlets
-			260647, -- Digested Human Hand
-			ns.rewards.Currency(ns.CURRENCY_SILVERMOONCOURT, 50, {quest=94685}),
-		},
-		vignette=7294,
-	},
 	[62964878] = { -- Cre'van
 		criteria=110170, quest=92391,
 		npc=250719,
@@ -259,4 +248,23 @@ ns.RegisterPoints(ns.EVERSONGWOODS, {
 		{257147, mount=true}, -- Cobalt Dragonhawk
 		{257156, mount=true}, -- Cerulean Hawkstrider
 	},
+})
+
+-- Bloated Snapdragon
+ns.RegisterPoints(ns.EVERSONGWOODS, {
+	-- Normal
+	[36566407] = {requires=ns.conditions.NotAreaPoi(ns.EVERSONGWOODS, 8758)},
+	-- Void invasions
+	[37696427] = {requires=ns.conditions.AreaPoi(ns.EVERSONGWOODS, 8758)},
+}, {
+	achievement=61507,
+	criteria=110169, quest=92366,
+	npc=250582,
+	loot={
+		264543, -- Snapdragon Pantaloons
+		264560, -- Sharpclaw Gauntlets
+		260647, -- Digested Human Hand
+		ns.rewards.Currency(ns.CURRENCY_SILVERMOONCOURT, 50, {quest=94685}),
+	},
+	vignette=7294,
 })

commit 8a7af5ab90e328a68b2538653413daf10df0bf17

Eversong Woods: restore shared loot to Bloated Snapdragon
Follow-up to a934612a7e460b389f060d7d57fe8a7008e4246f which accidentally
removed it.

zones/EversongWoods.lua
@@ -92,6 +92,13 @@ ns.RegisterPoints(ns.EVERSONGWOODS, {
 
 -- Rares
 
+local loot_shared={
+	251788, -- Gift of Light
+	251791, -- Holy Retributor's Order
+	{257147, mount=true}, -- Cobalt Dragonhawk
+	{257156, mount=true}, -- Cerulean Hawkstrider
+}
+
 -- A Bloody Song
 ns.RegisterPoints(ns.EVERSONGWOODS, {
 	[51397502] = { -- Warden of Weeds
@@ -242,19 +249,14 @@ ns.RegisterPoints(ns.EVERSONGWOODS, {
 	},
 }, {
 	achievement=61507,
-	loot_shared={
-		251788, -- Gift of Light
-		251791, -- Holy Retributor's Order
-		{257147, mount=true}, -- Cobalt Dragonhawk
-		{257156, mount=true}, -- Cerulean Hawkstrider
-	},
+	loot_shared=loot_shared,
 })
 
 -- Bloated Snapdragon
 ns.RegisterPoints(ns.EVERSONGWOODS, {
 	-- Normal
 	[36566407] = {requires=ns.conditions.NotAreaPoi(ns.EVERSONGWOODS, 8758)},
-	-- Void invasions
+	-- Void invasions (8615 for the ritual site would probably also work?)
 	[37696427] = {requires=ns.conditions.AreaPoi(ns.EVERSONGWOODS, 8758)},
 }, {
 	achievement=61507,
@@ -266,5 +268,6 @@ ns.RegisterPoints(ns.EVERSONGWOODS, {
 		260647, -- Digested Human Hand
 		ns.rewards.Currency(ns.CURRENCY_SILVERMOONCOURT, 50, {quest=94685}),
 	},
+	loot_shared=loot_shared,
 	vignette=7294,
 })